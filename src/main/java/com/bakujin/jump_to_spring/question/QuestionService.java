4ba9cd78b2bd97ad95dc87c6884ee6e186b03ffc
bakujinDev <87050600+bakujinDev@users.noreply.github.com>
2023-07-29 16:44:40 +0900

    create search

--- src/main/java/com/bakujin/jump_to_spring/question/QuestionService.java
+++ src/main/java/com/bakujin/jump_to_spring/question/QuestionService.java
@@ -9,17 +9,45 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.bakujin.jump_to_spring.DataNotFoundException;
+import com.bakujin.jump_to_spring.answer.Answer;
 import com.bakujin.jump_to_spring.user.SiteUser;
 
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
   private final QuestionRepository questionRepository;
+
+  private Specification<Question> search(String kw) {
+    return new Specification<>() {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+        query.distinct(true);
+        Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+        Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+        Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+        return cb.or(
+            cb.like(q.get("subject"), "%" + kw + "%"),
+            cb.like(q.get("content"), "%" + kw + "%"),
+            cb.like(u1.get("username"), "%" + kw + "%"),
+            cb.like(a.get("content"), "%" + kw + "%"),
+            cb.like(u2.get("username"), "%" + kw + "%"));
+      }
+    };
+  }
 
   public List<Question> getList() {
     return this.questionRepository.findAll();
@@ -43,11 +71,11 @@ public class QuestionService {
     this.questionRepository.save(q);
   }
 
-  public Page<Question> getList(int page) {
+  public Page<Question> getList(int page, String kw) {
     List<Sort.Order> sorts = new ArrayList<>();
     sorts.add(Sort.Order.desc("createDate"));
     Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-    return this.questionRepository.findAll(pageable);
+    return this.questionRepository.findAllByKeyword(kw, pageable);
   }
 
   public void delete(Question question) {
